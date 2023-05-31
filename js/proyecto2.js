838b570b97431b2e498c005492fa366dbf29da2b
Erik Casares <erik.casaresps@gmail.com>
2023-05-30 22:31:28 -0300

    segunda entrega

--- js/proyecto2.js
+++ js/proyecto2.js
@@ -13,24 +13,32 @@ class plan_de_ahorro{
     }
 //EXPONER LOS DATOS QUE PUSIERON LOS USUARIOS
     get_datos(){
+        if(isNaN(this.ahorro), isNaN(this.sueldo), isNaN(this.meses)){
+            console.log("no ha ingresado numeros")
+            alert("para calcular se tienen que ingresar numeros")
+            reinicio = prompt("queres volver a hacer un plan?")
+        }
+        else{
         console.log( "<--- DATOS DEL calculo --->");
         console.log("queres ahorrar; $", this.ahorro);
         console.log("contas con un ingreso mensual de; $", this.sueldo);
         console.log("queres cumplir tu meta en:", this.meses,"meses")
         console.log(" ")
+        }
     }
 // CALCULAR 
     get_calculo(){
-        if(this.meses>0){
+        if(isNaN(this.ahorro), isNaN(this.sueldo), isNaN(this.meses)){
+        }
+        else if(this.meses>0){
                 console.log("hola")
                 this.guardar = this.ahorro / this.meses; 
                 ;
-                console.log("deberias guardar",parseFloat(this.guardar),"por mes.");
+                console.log("deberias guardar",Math.ceil(this.guardar),"por mes.");
         }
         else{
             console.log("no ha ingresado numeros")
             alert("para calcular se tienen que ingresar numeros")
-            reinicio = prompt("queres volver a hacer un plan?")
         }
     }
     get_porcentaje(){
@@ -48,32 +56,18 @@ class plan_de_ahorro{
         }
         else if(this.guardar > this.sueldo){
             console.log("no es posible guardar mas que tu ingreso mensual, pensa en alcanzar la meta en un periodo mayor de tiempo")
+            console.log(" ")
         }
         else{"error"}
     }
-    get_otras_opciones(){/*
-        let opcion_3_meses = this.ahorro / 3
-        let opcion_6_meses = this.ahorro / 6
-        let opcion_9_meses = this.ahorro / 9
-        let opcion_12_meses = this.ahorro / 12 
-        let opcion_2_años = this.ahorro / 24
-        console.log(">>Otras opciones para llegar a la meta de ahorro<<")
-        console.log("Guardando" , opcion_3_meses , "$ durante 3 meses")
-        console.log("Guardando" , opcion_6_meses , "$ durante 6 meses")
-        console.log("Guardando" , opcion_9_meses , "$ durante 9 meses")
-        console.log("Guardando" , opcion_12_meses , "$ durante 12 meses")
-*/
+    get_otras_opciones(){
 
+if(isNaN(this.ahorro), isNaN(this.sueldo), isNaN(this.meses)){
 
+    
 
-
-
-
-
-
-
-
-
+}
+else{
         function tresMeses( num){
 
             return num / 3
@@ -109,9 +103,7 @@ class plan_de_ahorro{
         else if(opcion_3_meses < this.sueldo){
             console.log("se puede considerar un ahorro" , lista_posibilidades[1])
         }
-        else if(this.guardar > this.sueldo){
-            console.log("no es posible guardar mas que tu ingreso mensual, pensa en alcanzar la meta en un periodo mayor de tiempo")
-        }
+        else { console.log("se puede considerar un ahorro", lista_posibilidades[2])}
         console.log(" ")
         
         console.log("en seis meses deberias guardar", Math.ceil(opcion_6_meses),"$ por mes.");
@@ -129,9 +121,10 @@ class plan_de_ahorro{
         if((opcion_9_meses / this.sueldo) * 100 < 40){
             console.log("se puede considerar un ahorro" , lista_posibilidades[0])
         }
-        else{
+        else if (opcion_9_meses < this.sueldo){
             console.log("se puede considerar un ahorro" , lista_posibilidades[1])
         }
+        else { console.log("se puede considerar un ahorro", lista_posibilidades[2])}
         console.log(" ")
 
 
@@ -140,25 +133,28 @@ class plan_de_ahorro{
         if((opcion_ano / this.sueldo) * 100 < 40){
             console.log("se puede considerar un ahorro" , lista_posibilidades[0])
         }
-        else{
+        else if (opcion_ano < this.sueldo){
             console.log("se puede considerar un ahorro" , lista_posibilidades[1])
         }
+        else { console.log("se puede considerar un ahorro", lista_posibilidades[2])}
+        console.log(" ")
         console.log(" ")
         console.log("en dos años deberias guardar", Math.round(opcion_2_anos),"$ por mes.");
         if((opcion_2_anos / this.sueldo) * 100 < 40){
             console.log("se puede considerar un ahorro" , lista_posibilidades[0])
         }
-        else{
+        else if (opcion_2_anos < this.sueldo){
             console.log("se puede considerar un ahorro" , lista_posibilidades[1])
         }
+        else { console.log("se puede considerar un ahorro", lista_posibilidades[2])}
         console.log(" ")
         
     }
-    
+}
 } 
-let lista_posibilidades = ["posible" , "dificil" , "facil" ]
+let lista_posibilidades = ["posible" , "dificil" , "imposible" ]
 let reinicio = "si"
-
+let lista_ahorros = [];
 while(reinicio === "si"){
     let plan_1 = new plan_de_ahorro(prompt("cuanto queres ahorrar") , prompt("cuanto es tu ingreso mensual?") , prompt("en cuantos meses queres cumplir tu meta?"));
     plan_1.get_datos();
@@ -166,9 +162,45 @@ while(reinicio === "si"){
     plan_1.get_porcentaje();
     plan_1.get_otras_opciones();
     if((this.guardar > this.sueldo)){
-    plan_1.meses = (prompt("queres probar otra meta de meses?"))
+        plan_1.meses = (prompt("queres probar otra meta de meses?"))
     }
+    lista_ahorros.push(plan_1);
+
     reinicio = prompt("queres volver a hacer un plan?")
+
 }
+let listado = prompt("Deseas mostrar el historial de tus planes?")
+while(listado === 'si'){
+if(listado === 'si'){
+    mostrarListado();
+}
+
+
+function mostrarListado(){
+    let opcion = prompt("ingresa 1 si deseas ver los posibles, 2 no posibles, 3 todos")
+    if(opcion == 1){
+        console.log("Listado Posibles");
+        let posibles = lista_ahorros.filter(ahorro => ahorro.guardar < ahorro.sueldo);
+        posibles.forEach(ahorro => {
+            ahorro.get_datos();
+        })
+    }else if(opcion == 2){
+        console.log("Listado No Posibles");
+        let posibles = lista_ahorros.filter(ahorro => ahorro.guardar > ahorro.sueldo);
+        posibles.forEach(ahorro => {
+            ahorro.get_datos();
+        })
+    }else if (opcion == 3){
+        console.log("Listado General");
+        lista_ahorros.forEach(ahorro => {
+            ahorro.get_datos();
+        })
+    }else {
+        alert('opcion Incorrecto');
+    }
+}
+listado = prompt("Deseas ver otro historial de tus planes?")
+}
+
 alert("Saliste del Sistema")
 console.log("Gracias por utilizar nuestro sistema")
